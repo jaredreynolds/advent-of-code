428f31aa2ab700bd85bc913cd8df8bf443d91d87
Jared Reynolds <jared.reynolds@changepoint.com>
2021-12-07 00:37:41 -0800

    2021-07 crab fuel

---
+++ 2021/07/07A/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Text;
+using CrabFuel;
+using static CrabFuel.Utils;
+
+var input = (new string[] {
+    "16,1,2,0,4,2,7,1,2,14"
+});
+
+var filename = args.Length > 0 ? args[0] : "../input.txt";
+if (File.Exists(filename)) {
+    input = File.ReadAllLines(filename);
+} else {
+    WriteLine(ConsoleColor.Yellow, "Input file not specified or found; using short sample data.");
+}
+
+var crabLoc = input
+    .First()
+    .Split(',')
+    .Select(c => Int32.Parse(c))
+    .ToArray();
+
+int minCost = Int32.MaxValue;
+int minTarget = -1;
+int minLoc = crabLoc.Min();
+int maxLoc = crabLoc.Max();
+
+for (int target = minLoc; target <= maxLoc; target++)
+{
+    var cost = crabLoc.Sum(loc => Math.Abs(target - loc));
+
+    if (cost < minCost)
+    {
+        minCost = cost;
+        minTarget = target;
+    }
+}
+
+System.Console.WriteLine($"Target: {minTarget} for {minCost} fuel");
---
+++ 2021/07/07B/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Text;
+using CrabFuel;
+using static CrabFuel.Utils;
+
+var input = (new string[] {
+    "16,1,2,0,4,2,7,1,2,14"
+});
+
+var filename = args.Length > 0 ? args[0] : "../input.txt";
+if (File.Exists(filename)) {
+    input = File.ReadAllLines(filename);
+} else {
+    WriteLine(ConsoleColor.Yellow, "Input file not specified or found; using short sample data.");
+}
+
+var crabLoc = input
+    .First()
+    .Split(',')
+    .Select(c => Int32.Parse(c))
+    .ToArray();
+
+int minCost = Int32.MaxValue;
+int minTarget = -1;
+int minLoc = crabLoc.Min();
+int maxLoc = crabLoc.Max();
+
+for (int target = minLoc; target <= maxLoc; target++)
+{
+    var cost = crabLoc.Sum(loc =>
+    {
+        var steps = Math.Abs(target - loc);
+        return (steps * (steps + 1)) / 2; // summation: n(n+1)/2
+    });
+
+    if (cost < minCost)
+    {
+        minCost = cost;
+        minTarget = target;
+    }
+}
+
+System.Console.WriteLine($"Target: {minTarget} for {minCost} fuel");
